810c8929e404b7d18d6fb00df8d54a07d5abe522
Devraj Bagchi <168226995+chemlord@users.noreply.github.com>
2024-12-28 07:28:34 +0000

    Quick Location Fix

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -8,7 +8,7 @@ const person = {
     },
     role:      'Biochemistry and Molecular Biology',
     avatar:    '/images/avatar.jpg',
-    location:  'America/Toronto',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+    location:  'Peterborough, Ontario',        // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
     languages: []  // optional: Leave the array empty if you don't want to display languages
 }
 
