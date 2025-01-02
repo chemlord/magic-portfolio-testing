30976519b0d679e3083955f4c53b77ec2ef51322
Devraj Bagchi <168226995+chemlord@users.noreply.github.com>
2025-01-02 02:30:36 +0000

    another test

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -140,6 +140,9 @@ export default function Home(
 
 			
 			</Flex>
+			<RevealFx translateY="16" delay={0.6}>
+				<Projects range={[1]} locale={locale}/>
+			</RevealFx>
 			{routes['/blog'] && (
 				<Flex
 					fillWidth gap="24"
