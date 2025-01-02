2061c80ee4d4511095710bf73342003f26db78aa
Devraj Bagchi <168226995+chemlord@users.noreply.github.com>
2025-01-02 02:26:48 +0000

    Anotherone

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -140,8 +140,25 @@ export default function Home(
 
 			
 			</Flex>
+			{routes['/blog'] && (
+				<Flex
+					fillWidth gap="24"
+					mobileDirection="column">
+					<Flex flex={1} paddingLeft="l">
+						<Heading
+							as="h2"
+							variant="display-strong-xs"
+							wrap="balance">
+							Latest from the blog
+						</Heading>
+					</Flex>
+					<Flex
+						flex={3} paddingX="20">
+						<Posts range={[1,3]} columns="3" locale={locale}/>
+					</Flex>
+				</Flex>
+			)}
 			<RevealFx translateY="16" delay={0.6}>
-				<Projects range={[1]} locale={locale}/>
 				<Projects range={[1]} locale={locale}/>
 				{ newsletter.display &&
 				<Mailchimp newsletter={newsletter} />
